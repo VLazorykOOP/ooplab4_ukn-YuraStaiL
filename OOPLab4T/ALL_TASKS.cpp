fe1d0bc2d33d4714e334819f4346487a4e603ada
YuraStaiL <amator111555@gmail.com>
2022-12-12 12:06:15 +0200

    reconstruct

--- OOPLab4T/ALL_TASKS.cpp
+++ OOPLab4T/ALL_TASKS.cpp
@@ -10,7 +10,7 @@ enum STATE {
 class VectorDouble
 {
 	double* vec;
-	int num = 2;
+	int size = 2;
 	int state = 0;
 	static double bad_index;
 public:
@@ -69,115 +69,115 @@ public:
 double VectorDouble::bad_index = 0;
 VectorDouble::VectorDouble(int n) {
 	if (n <= 0)    n = 1;
-	num = n;
+	size = n;
 	vec = new double[n];
 	for (int i = 0; i < n; i++) {
 		vec[i] = 0.0;
 	}
 }
 VectorDouble::VectorDouble(int n, double b) : VectorDouble(n) {
-	for (int i = 0; i < num; i++) {
+	for (int i = 0; i < size; i++) {
 		vec[i] = b;
 	}
 }
 VectorDouble::VectorDouble(const VectorDouble& s) {
-	num = s.num;
-	vec = new double[num];
+	size = s.size;
+	vec = new double[size];
 	state = 1;
-	for (int i = 0; i < num; i++)   vec[i] = s.vec[i];
+	for (int i = 0; i < size; i++)   vec[i] = s.vec[i];
 }
 VectorDouble& VectorDouble::operator=(const VectorDouble& s) {
 	if (this == &s) return *this;
-	if (num != s.num)
+	if (size != s.size)
 	{
 		if (vec) delete[] vec;
-		num = s.num;
-		vec = new double[num];
+		size = s.size;
+		vec = new double[size];
 		state = 1;
 	}
-	for (int i = 0; i < num; i++)   vec[i] = s.vec[i];
+	for (int i = 0; i < size; i++)   vec[i] = s.vec[i];
 	return *this;
 }
 VectorDouble& VectorDouble::operator=(VectorDouble&& s) noexcept
 {
 	if (this == &s) return *this;
-	num = s.num; state = s.state;
+	size = s.size; state = s.state;
 	if (this->vec != nullptr) delete[] vec;
 	vec = s.vec;
-	s.num = 0; s.vec = nullptr; s.state = -1;
+	s.size = 0; s.vec = nullptr; s.state = -1;
 	return *this;
 }
 void VectorDouble::Init(int n) {
-	if (num != n) {
+	if (size != n) {
 		if (vec != nullptr) delete[] vec;
-		num = n;
+		size = n;
 		vec = new double[n];
 	}
-	for (int i = 0; i < num; i++) 	vec[i] = 0;
+	for (int i = 0; i < size; i++) 	vec[i] = 0;
 }
 void VectorDouble::Init(int n, double b) {
-	if (num != n) {
+	if (size != n) {
 		if (vec != nullptr) delete[] vec;
-		num = n;
+		size = n;
 		vec = new double[n];
 	}
-	for (int i = 0; i < num; i++) 	vec[i] = b;
+	for (int i = 0; i < size; i++) 	vec[i] = b;
 }
 istream& operator>>(istream& is, VectorDouble& s) {
-	for (int i = 0; i < s.num; i++)
+	for (int i = 0; i < s.size; i++)
 	{
 		is >> s.vec[i];
 	}
 	return is;
 };
 ostream& operator<<(ostream& os, VectorDouble& s) {
-	for (int i = 0; i < s.num; i++)
+	for (int i = 0; i < s.size; i++)
 		os << s.vec[i] << '\t';
 	os << endl;
 	return os;
 }
 double& VectorDouble::operator[](int index)
 {
-	if (index >= 0 && index < num) return vec[index];
+	if (index >= 0 && index < size) return vec[index];
 	cout << " Error : operator[] - index out of range \n";
 	return bad_index;
 }
 VectorDouble& VectorDouble::operator-=(const VectorDouble& b)
 {
 	int i;
-	if (num == b.num) for (i = 0; i < num; i++) vec[i] -= b.vec[i];
+	if (size == b.size) for (i = 0; i < size; i++) vec[i] -= b.vec[i];
 	else {
 		cout << " Warning: vectors of different sizes are used in operation -= \n";
-		if (num < b.num) {
+		if (size < b.size) {
 			double* tmp;
-			tmp = new double[b.num];
-			for (i = 0; i < num; i++) tmp[i] = vec[i] - b.vec[i];
-			for (; i < b.num; i++) tmp[i] = -b.vec[i];
-			num = b.num;
+			tmp = new double[b.size];
+			for (i = 0; i < size; i++) tmp[i] = vec[i] - b.vec[i];
+			for (; i < b.size; i++) tmp[i] = -b.vec[i];
+			size = b.size;
 			if (vec != nullptr) delete[] vec;
 			vec = tmp;
 		}
-		else 	for (i = 0; i < b.num; i++) vec[i] -= b.vec[i];
+		else 	for (i = 0; i < b.size; i++) vec[i] -= b.vec[i];
 	}
 	return *this;
 }
 VectorDouble& VectorDouble::operator-=(const double& b)
 {
-	if (num > 0) {
-		for (int i = 0; i < num; i++) vec[i] -= b;
+	if (size > 0) {
+		for (int i = 0; i < size; i++) vec[i] -= b;
 	}
 	return *this;
 }
 VectorDouble& VectorDouble::operator-=(const float& b)
 {
-	if (num > 0) {
-		for (int i = 0; i < num; i++) vec[i] -= b;
+	if (size > 0) {
+		for (int i = 0; i < size; i++) vec[i] -= b;
 	}
 	return *this;
 }
 VectorDouble& VectorDouble::operator--()
 {
-	for (int i = 0; i < num; i++) vec[i] -= 1;
+	for (int i = 0; i < size; i++) vec[i] -= 1;
 	return *this;
 }
 VectorDouble VectorDouble::operator-(const VectorDouble& b)
@@ -200,40 +200,40 @@ VectorDouble VectorDouble::operator-(const float& b)
 }
 VectorDouble& VectorDouble::operator++()
 {
-	for (int i = 0; i < num; i++) vec[i] += 1;
+	for (int i = 0; i < size; i++) vec[i] += 1;
 	return *this;
 }
 VectorDouble& VectorDouble::operator+=(const VectorDouble& b)
 {
 	int i;
-	if (num == b.num) for (i = 0; i < num; i++) vec[i] += b.vec[i];
+	if (size == b.size) for (i = 0; i < size; i++) vec[i] += b.vec[i];
 	else
 	{
 		cout << " Warning: vectors of different sizes are used in operation += \n";
-		if (num < b.num) {
+		if (size < b.size) {
 			double* tmp;
-			tmp = new double[b.num];
-			for (i = 0; i < num; i++) tmp[i] = vec[i] + b.vec[i];
-			for (; i < b.num; i++) tmp[i] = b.vec[i];
-			num = b.num;
+			tmp = new double[b.size];
+			for (i = 0; i < size; i++) tmp[i] = vec[i] + b.vec[i];
+			for (; i < b.size; i++) tmp[i] = b.vec[i];
+			size = b.size;
 			if (vec != nullptr) delete[] vec;
 			vec = tmp;
 		}
-		else for (i = 0; i < b.num; i++) vec[i] += b.vec[i];
+		else for (i = 0; i < b.size; i++) vec[i] += b.vec[i];
 	}
 	return *this;
 }
 VectorDouble& VectorDouble::operator+=(const double& b)
 {
-	if (num > 0) {
-		for (int i = 0; i < num; i++) vec[i] += b;
+	if (size > 0) {
+		for (int i = 0; i < size; i++) vec[i] += b;
 	}
 	return *this;
 }
 VectorDouble& VectorDouble::operator+=(const float& b)
 {
-	if (num > 0) {
-		for (int i = 0; i < num; i++) vec[i] += b;
+	if (size > 0) {
+		for (int i = 0; i < size; i++) vec[i] += b;
 	}
 	return *this;
 }
@@ -257,8 +257,8 @@ VectorDouble VectorDouble::operator+(const float& b)
 }
 VectorDouble& VectorDouble::operator*=(const  VectorDouble& b)
 {
-	if (num != b.num) { std::cout << " Error : vectors of different sizes are used in operation %= \n"; return *this; }
-	for (int i = 0; i < num; i++) vec[i] *= b.vec[i];
+	if (size != b.size) { std::cout << " Error : vectors of different sizes are used in operation %= \n"; return *this; }
+	for (int i = 0; i < size; i++) vec[i] *= b.vec[i];
 	return *this;
 }
 VectorDouble& VectorDouble::operator*=(const double& b) {
@@ -268,7 +268,7 @@ VectorDouble& VectorDouble::operator*=(const double& b) {
 		cout << "Eror BAD_DIV" << endl;
 		return *this;
 	}
-	for (int i = 0; i < num; i++) vec[i] *= b;
+	for (int i = 0; i < size; i++) vec[i] *= b;
 	return *this;
 	return tmp;
 }
@@ -279,14 +279,14 @@ VectorDouble& VectorDouble::operator*=(const float& b) {
 		cout << "Eror BAD_DIV" << endl;
 		return *this;
 	}
-	for (int i = 0; i < num; i++) vec[i] *= b;
+	for (int i = 0; i < size; i++) vec[i] *= b;
 	return *this;
 	return tmp;
 }
 VectorDouble VectorDouble::operator*(const  VectorDouble& b)
 {
-	if (num != b.num) { std::cout << " Error : vectors of different sizes are used in operation %= \n"; return *this; }
-	for (int i = 0; i < num; i++) vec[i] *= b.vec[i];
+	if (size != b.size) { std::cout << " Error : vectors of different sizes are used in operation %= \n"; return *this; }
+	for (int i = 0; i < size; i++) vec[i] *= b.vec[i];
 	return *this;
 }
 VectorDouble VectorDouble::operator*(const double& b) {
@@ -315,8 +315,8 @@ VectorDouble VectorDouble::operator*(const float& b) {
 }
 VectorDouble& VectorDouble::operator/=(const  VectorDouble& b)
 {
-	if (num != b.num) { std::cout << " Error : vectors of different sizes are used in operation %= \n"; return *this; }
-	for (int i = 0; i < num; i++) vec[i] /= b.vec[i];
+	if (size != b.size) { std::cout << " Error : vectors of different sizes are used in operation %= \n"; return *this; }
+	for (int i = 0; i < size; i++) vec[i] /= b.vec[i];
 	return *this;
 }
 VectorDouble& VectorDouble::operator/=(const double& b) {
@@ -326,7 +326,7 @@ VectorDouble& VectorDouble::operator/=(const double& b) {
 		cout << "Eror BAD_DIV" << endl;
 		return *this;
 	}
-	else for (int i = 0; i < num; i++) vec[i] /= b;
+	else for (int i = 0; i < size; i++) vec[i] /= b;
 	return *this;
 	return tmp;
 }
@@ -337,7 +337,7 @@ VectorDouble& VectorDouble::operator/=(const float& b) {
 		cout << "Eror BAD_DIV" << endl;
 		return *this;
 	}
-	else for (int i = 0; i < num; i++) vec[i] /= b;
+	else for (int i = 0; i < size; i++) vec[i] /= b;
 	return *this;
 	return tmp;
 }
@@ -345,8 +345,8 @@ VectorDouble& VectorDouble::operator/=(const float& b) {
 
 VectorDouble& VectorDouble::operator/(const  VectorDouble& b)
 {
-	if (num != b.num) { std::cout << " Error : vectors of different sizes are used in operation %= \n"; return *this; }
-	for (int i = 0; i < num; i++) vec[i] /= b.vec[i];
+	if (size != b.size) { std::cout << " Error : vectors of different sizes are used in operation %= \n"; return *this; }
+	for (int i = 0; i < size; i++) vec[i] /= b.vec[i];
 	return *this;
 }
 VectorDouble VectorDouble::operator/(const double& b) {
@@ -374,28 +374,28 @@ VectorDouble VectorDouble::operator/(const float& b) {
 	return tmp;
 }
 bool VectorDouble::operator>(const VectorDouble& s) {
-	for (int i = 0; i < num; i++) {
+	for (int i = 0; i < size; i++) {
 		if (vec[i] > s.vec[i])
 			return true;
 	}
 	return false;
 }
 bool VectorDouble::operator>=(const VectorDouble& s) {
-	for (int i = 0; i < num; i++) {
+	for (int i = 0; i < size; i++) {
 		if (vec[i] >= s.vec[i])
 			return true;
 	}
 	return false;
 }
 bool VectorDouble::operator<(const VectorDouble& s) {
-	for (int i = 0; i < num; i++) {
+	for (int i = 0; i < size; i++) {
 		if (vec[i] < s.vec[i])
 			return true;
 	}
 	return false;
 }
 bool VectorDouble::operator<=(const VectorDouble& s) {
-	for (int i = 0; i < num; i++) {
+	for (int i = 0; i < size; i++) {
 		if (vec[i] <= s.vec[i])
 			return true;
 	}
@@ -444,7 +444,7 @@ void task1() {
 	cout << "***Перевантаження vector / vector:" << endl << endl;
 	vector2 /= vector3;
 	cout << vector2 << endl;
-	cout << "***Перевантаження vector * 0:" << endl << endl;
+	cout << "***Перевантаження vector * 1.5:" << endl << endl;
 	vector2 *= 1.5;
 	cout << vector2 << endl;
 	
@@ -453,20 +453,20 @@ void task1() {
 //       Друге завдання
 class MatrixDouble {
 private:
-	VectorDouble* arr = nullptr;
+	VectorDouble* DoubleArray = nullptr;
 	int n = 1;
-	int m = 1;
+	int size = 1;
 	int state = 0;
 public:
-	MatrixDouble() :arr(nullptr), n(0), m(0), state(0) {}
+	MatrixDouble() :DoubleArray(nullptr), n(0), size(0), state(0) {}
 	MatrixDouble(int n) :MatrixDouble(n, n) {};
-	MatrixDouble(int n, int m) :MatrixDouble(n, m, 0) {};
-	MatrixDouble(int n, int m, float);
+	MatrixDouble(int n, int size) :MatrixDouble(n, size, 0) {};
+	MatrixDouble(int n, int size, float);
 	MatrixDouble(const MatrixDouble& s);
 	MatrixDouble& operator=(const MatrixDouble& s);
 	MatrixDouble& operator=(MatrixDouble&& s) noexcept;
 	~MatrixDouble() {
-		if (arr) delete[] arr;
+		if (DoubleArray) delete[] DoubleArray;
 	}
 	friend istream& operator>>(istream& is, MatrixDouble& s);
 	friend ostream& operator<<(ostream& os, MatrixDouble& s);
@@ -511,20 +511,20 @@ public:
 MatrixDouble::MatrixDouble(int ni, int mi, float b)
 {
 	if (ni <= 0) n = 1; else n = ni;
-	if (mi <= 0) m = 1; else m = mi;
-	arr = new VectorDouble[n];
-	for (int i = 0; i < n; i++) arr[i].Init(m, b);
+	if (mi <= 0) size = 1; else size = mi;
+	DoubleArray = new VectorDouble[n];
+	for (int i = 0; i < n; i++) DoubleArray[i].Init(size, b);
 }
 
 MatrixDouble::MatrixDouble(const MatrixDouble& s)
 {
 	n = s.n;
-	m = s.m;
+	size = s.size;
 	int i;
-	arr = new VectorDouble[n];
-	for (i = 0; i < n; i++) arr[i].Init(m, 0);
+	DoubleArray = new VectorDouble[n];
+	for (i = 0; i < n; i++) DoubleArray[i].Init(size, 0);
 	for (i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)  arr[i][j] = s.arr[i][j];
+		for (int j = 0; j < size; j++)  DoubleArray[i][j] = s.DoubleArray[i][j];
 
 }
 
@@ -532,64 +532,64 @@ MatrixDouble& MatrixDouble::operator=(const MatrixDouble& s)
 {
 	if (this != &s) {
 		int i;
-		if (n != s.n || m != s.m) {
-			n = s.n; m = s.m;
-			if (arr != nullptr) {
-				delete[] arr;
+		if (n != s.n || size != s.size) {
+			n = s.n; size = s.size;
+			if (DoubleArray != nullptr) {
+				delete[] DoubleArray;
 			}
-			arr = new VectorDouble[n];
-			for (i = 0; i < n; i++) arr[i].Init(m, 0);
+			DoubleArray = new VectorDouble[n];
+			for (i = 0; i < n; i++) DoubleArray[i].Init(size, 0);
 		}
 		for (i = 0; i < n; i++)
-			for (int j = 0; j < m; j++)  arr[i][j] = s.arr[i][j];
+			for (int j = 0; j < size; j++)  DoubleArray[i][j] = s.DoubleArray[i][j];
 
 	}
 	return *this;
 }
 VectorDouble& MatrixDouble::operator[](int index)
 {
-	if (index >= 0 && index < n) return arr[index];
+	if (index >= 0 && index < n) return DoubleArray[index];
 	cout << " Error : operator[] - index index out of range \n";
-	return arr[0];
+	return DoubleArray[0];
 }
 
 MatrixDouble& MatrixDouble::operator=(MatrixDouble&& s) noexcept
 {
-	n = s.n; m = s.m;
-	arr = s.arr;
-	s.arr = nullptr;
-	s.n = 0; s.m = 0;
+	n = s.n; size = s.size;
+	DoubleArray = s.DoubleArray;
+	s.DoubleArray = nullptr;
+	s.n = 0; s.size = 0;
 	return *this;
 }
 istream& operator>>(istream& is, MatrixDouble& s)
 {
-	for (int i = 0; i < s.n; i++) is >> s.arr[i];
+	for (int i = 0; i < s.n; i++) is >> s.DoubleArray[i];
 	return is;
 }
 
 ostream& operator<<(ostream& os, MatrixDouble& s)
 {
-	for (int i = 0; i < s.n; i++) os << s.arr[i];  // << endl;
+	for (int i = 0; i < s.n; i++) os << s.DoubleArray[i];  // << endl;
 	return os;
 }
 MatrixDouble& MatrixDouble::operator++() {
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			arr[i][j] += 1;
+		for (int j = 0; j < size; j++) {
+			DoubleArray[i][j] += 1;
 		}
 	}
 	return *this;
 }
 MatrixDouble& MatrixDouble::operator--() {
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			arr[i][j] -= 1;
+		for (int j = 0; j < size; j++) {
+			DoubleArray[i][j] -= 1;
 		}
 	}
 	return *this;
 }
 MatrixDouble& MatrixDouble::operator+=(const MatrixDouble& s) {
-	if (s.n == n) for (int i = 0; i < n; i++) arr[i] += s.arr[i];
+	if (s.n == n) for (int i = 0; i < n; i++) DoubleArray[i] += s.DoubleArray[i];
 	else {
 		cout << "Error: matrices of different dimensions \n";
 		cout << "The += operation was not performed. \n";
@@ -599,8 +599,8 @@ MatrixDouble& MatrixDouble::operator+=(const MatrixDouble& s) {
 
 MatrixDouble& MatrixDouble::operator+=(const float& b) {
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			arr[i][j] += b;
+		for (int j = 0; j < size; j++) {
+			DoubleArray[i][j] += b;
 		}
 	}
 	return *this;
@@ -628,7 +628,7 @@ MatrixDouble  MatrixDouble::operator+(const float& b) {
 	return mat;
 }
 MatrixDouble& MatrixDouble::operator-=(const MatrixDouble& s) {
-	if (s.n == n) for (int i = 0; i < n; i++) arr[i] -= s.arr[i];
+	if (s.n == n) for (int i = 0; i < n; i++) DoubleArray[i] -= s.DoubleArray[i];
 	else {
 		cout << "Error: matrices of different dimensions \n";
 		cout << "The -= operation was not performed. \n";
@@ -636,11 +636,11 @@ MatrixDouble& MatrixDouble::operator-=(const MatrixDouble& s) {
 	return *this;
 }
 MatrixDouble& MatrixDouble::operator-=(const double& b) {
-	for (int i = 0; i < n; i++) arr[i] -= b;
+	for (int i = 0; i < n; i++) DoubleArray[i] -= b;
 	return *this;
 }
 MatrixDouble& MatrixDouble::operator-=(const float& b) {
-	for (int i = 0; i < n; i++) arr[i] -= b;
+	for (int i = 0; i < n; i++) DoubleArray[i] -= b;
 	return *this;
 }
 MatrixDouble  MatrixDouble::operator-(const MatrixDouble& b) {
@@ -665,7 +665,7 @@ MatrixDouble& MatrixDouble::operator*=(const double& b) {
 		cout << "Error Multiply" << endl;
 		return *this;
 	}
-	else for (int i = 0; i < n; i++) arr[i] *= b;
+	else for (int i = 0; i < n; i++) DoubleArray[i] *= b;
 	return *this;
 	return tmp;
 }
@@ -676,18 +676,18 @@ MatrixDouble& MatrixDouble::operator*=(const float& b) {
 		cout << "Error Multiply" << endl;
 		return *this;
 	}
-	else for (int i = 0; i < n; i++) arr[i] *= b;
+	else for (int i = 0; i < n; i++) DoubleArray[i] *= b;
 	return *this;
 	return tmp;
 }
 MatrixDouble  MatrixDouble::operator*(const MatrixDouble& b) {
-	if (n == b.m && m == b.m) {
-		MatrixDouble ret(m, n);
+	if (n == b.size && size == b.size) {
+		MatrixDouble ret(size, n);
 		for (int i = 0; i < n; i++)
-			for (int j = 0; j < m; j++)
+			for (int j = 0; j < size; j++)
 			{
 				ret[i][j] = 0;
-				for (int l = 0; l < m; l++) ret[i][j] += arr[i][l] * b.arr[l][j];
+				for (int l = 0; l < size; l++) ret[i][j] += DoubleArray[i][l] * b.DoubleArray[l][j];
 			}
 		return ret;
 	}
@@ -717,7 +717,7 @@ MatrixDouble& MatrixDouble::operator/=(const double& b) {
 		cout << "Eror BAD_DIV" << endl;
 		return *this;
 	}
-	else for (int i = 0; i < n; i++) arr[i] /= b;
+	else for (int i = 0; i < n; i++) DoubleArray[i] /= b;
 	return *this;
 	return tmp;
 }
@@ -729,7 +729,7 @@ MatrixDouble& MatrixDouble::operator/=(const float& b) {
 		cout << "Eror BAD_DIV" << endl;
 		return *this;
 	}
-	else for (int i = 0; i < n; i++) arr[i] /= b;
+	else for (int i = 0; i < n; i++) DoubleArray[i] /= b;
 	return *this;
 	return tmp;
 }
@@ -742,7 +742,7 @@ MatrixDouble  MatrixDouble::operator/(const double& b) {
 		cout << "Eror BAD_DIV" << endl;
 		return mat;
 	}
-	else for (int i = 0; i < n; i++) arr[i] /= b;
+	else for (int i = 0; i < n; i++) DoubleArray[i] /= b;
 	return mat;
 	return tmp;
 }
@@ -755,14 +755,14 @@ MatrixDouble  MatrixDouble::operator/(const float& b) {
 		cout << "Eror BAD_DIV" << endl;
 		return mat;
 	}
-	else for (int i = 0; i < n; i++) arr[i] /= b;
+	else for (int i = 0; i < n; i++) DoubleArray[i] /= b;
 	return mat;
 	return tmp;
 }
 bool MatrixDouble::operator>(const  MatrixDouble& s) {
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] > s.arr[i][j])
+		for (int j = 0; j < size; j++) {
+			if (DoubleArray[i][j] > s.DoubleArray[i][j])
 				return true;
 		}
 	}
@@ -770,8 +770,8 @@ bool MatrixDouble::operator>(const  MatrixDouble& s) {
 }
 bool MatrixDouble::operator>=(const MatrixDouble& s) {
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] >= s.arr[i][j])
+		for (int j = 0; j < size; j++) {
+			if (DoubleArray[i][j] >= s.DoubleArray[i][j])
 				return true;
 		}
 	}
@@ -779,8 +779,8 @@ bool MatrixDouble::operator>=(const MatrixDouble& s) {
 }
 bool MatrixDouble::operator<(const  MatrixDouble& s) {
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] < s.arr[i][j])
+		for (int j = 0; j < size; j++) {
+			if (DoubleArray[i][j] < s.DoubleArray[i][j])
 				return true;
 		}
 	}
@@ -788,8 +788,8 @@ bool MatrixDouble::operator<(const  MatrixDouble& s) {
 }
 bool MatrixDouble::operator<=(const MatrixDouble& s) {
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] >= s.arr[i][j])
+		for (int j = 0; j < size; j++) {
+			if (DoubleArray[i][j] >= s.DoubleArray[i][j])
 				return true;
 		}
 	}
@@ -808,10 +808,10 @@ void task2() {
 	VectorDouble vector2(3);
 	double value = 1.7;
 	cout << "Task2 MatrixDouble : " << endl;
-	cout << "A" << endl << A << endl;
-	cout << "B" << endl << B << endl;
-	cout << "C" << endl << C << endl;
-	cout << "D" << endl << D << endl;
+	cout << "A ---" << endl << A << endl;
+	cout << "B ---" << endl << B << endl;
+	cout << "C ---" << endl << C << endl;
+	cout << "D ---" << endl << D << endl;
 	cout << "Вектор" << endl << vector1 << endl;
 	cout << "Операції:" << endl;
 	cout << "***Перевантаження ++::" << endl;
@@ -846,8 +846,15 @@ void task2() {
 	C /= 0.0;
 	cout << C;
 	cout << "Вираз 1" << endl;
-	cout << "C = C + D  - B  + C / 7 : " << endl;
+
+	cout << "cout << C " << endl << C << endl;
+	cout << "cout << D " << endl << D << endl;
+	cout << "cout << B " << endl << B << endl;
+		
+	cout << "C = (C + D  - B  + C) / 7 : " << endl;
 	C = (C + D - B + C) / 7.0;
+	C /= 7.0;
+	cout << "C /= 7.0" << endl;
 	cout << C;
 }
 
